9dd15b52352b6755c3b67d9b0f6c3fe9b09bbca9
awmleer <awmleer@sparker.xyz>
2017-06-26 18:17:49 +0800

    add: drag & drop

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+    @ViewChild('holder') holderRef : ElementRef;
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        let holder=this.holderRef.nativeElement;
+        console.log(holder);
+        holder.ondragover = () => {
+            return false;
+        };
+        holder.ondragleave = holder.ondragend = () => {
+            return false;
+        };
+        holder.ondrop = (e) => {
+            e.preventDefault();
+            for (let f of e.dataTransfer.files) {
+                console.log('File(s) you dragged here: ', f.path)
+            }
+            return false;
+        }
+    }
 
 }

dbf0de2d80cd6370fcc4915e46bb6885696a60f4
awmleer <awmleer@sparker.xyz>
2017-07-01 14:26:01 +0800

    [fix] remove some js files from git repo

--- tasks/build.default.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var gulp = require("gulp");
-/**
- * The default build task - sets up the underlying watch task
- */
-gulp.task('build.default', ['build.watch']);
--- tasks/build.restart.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var gulp = require("gulp");
-var electron_1 = require("./utils/electron");
-/**
- * Restarts the current electron window instance with the changes
- */
-gulp.task('build.restart', electron_1.electron.restart);

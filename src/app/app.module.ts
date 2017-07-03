6e55098f3c9eee0be5de7716427b383e57536d4b
awmleer <awmleer@sparker.xyz>
2017-07-03 11:16:25 +0800

    [fix] (IMPORTANT) sanitize html to prevent xss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PresentationPageComponent } from './pages/presentation-page/presentation-page.component';
 import { MarkdownToHtmlPipe } from './pipes/markdown-to-html.pipe';
 import {NgxElectronService} from "./services/ngx-electron/ngx-electron.service";
+import { SanitizeHtmlPipe } from './pipes/sanitize-html.pipe';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomePageComponent,
         PresentationPageComponent,
-        MarkdownToHtmlPipe
+        MarkdownToHtmlPipe,
+        SanitizeHtmlPipe
     ],
     imports: [
         BrowserModule,
